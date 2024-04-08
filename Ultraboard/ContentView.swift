4eb7555044c3ee95aa2d010a219d295044ed0f03
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2024-04-07 21:07:19 -0700

    backup

--- Ultraboard/ContentView.swift
+++ Ultraboard/ContentView.swift
@@ -10,13 +10,14 @@ struct RectangleButton: View {
     @Binding var isLocked: Bool
     @Binding var selectedButtonNames: [String]
     var client: TCPClient
+    @Binding var isSpecial: Bool
     @State private var holdDown = false
     var body: some View {
         GeometryReader { gp in
             ZStack {
                 Button(action: {
 //                    self.selectedButtonNames.append(self.name)
-//                    client.send(message: self.name + "press")
+//                    client.send(message: self.name)
 //                    let generator = UIImpactFeedbackGenerator(style: .medium)
 //                    generator.impactOccurred()
                 }) {
@@ -36,15 +37,21 @@ struct RectangleButton: View {
                         .onChanged { value in
                             self.position = isLocked ? self.position : value.location // Update dragAmount
                             if isLocked && !holdDown{
-                                print("lock down")
+                                self.selectedButtonNames.append(self.name)
+                                client.send(message: self.name)
+                                let generator = UIImpactFeedbackGenerator(style: .medium)
+                                generator.impactOccurred()
                                 holdDown = true
                             }
                         }
                         .onEnded {value in
-                            if isLocked {
-                                print("lock up")
-                                holdDown = false
+                            if isLocked && isSpecial {
+                                self.selectedButtonNames.append(self.name)
+                                client.send(message: self.name+"_end")
+                                let generator = UIImpactFeedbackGenerator(style: .medium)
+                                generator.impactOccurred()
                             }
+                            holdDown = false
                         }
                 )
                 .rotationEffect(rotationAngle)
@@ -96,10 +103,13 @@ struct ContentView: View {
     @State private var widthText: String = "100"
     @State private var heightText: String = "50"
     @State private var newName: String = ""
-    @State private var isPopoverPresented = false
+    @State private var isPopoverPresented1 = false
+    @State private var isPopoverPresented2 = false
     @State private var isLocked = false
     @State private var selectedButtonNames: [String] = []
     @State private var singleLineText: String = ""
+    @State private var selectedOption = 0 // Default selection
+    let names = ["Command", "Shift", "Option", "Control"]
     private let documentPickerDelegate = DocumentPickerDelegate()
     private let saveDocumentPickerDelegate = SaveDocumentPickerDelegate()
     private let client = TCPClient(host: "127.0.0.1", port: 1000)
@@ -111,9 +121,10 @@ struct ContentView: View {
         var name: String
         var position: CGPoint
         var rotationAngle: Angle // Include rotation angle
+        var isSpecial: Bool
         
         private enum CodingKeys: String, CodingKey {
-            case id, size, name, position, rotationAngle
+            case id, size, name, position, rotationAngle, isSpecial
         }
         
         func encode(to encoder: Encoder) throws {
@@ -123,14 +134,16 @@ struct ContentView: View {
             try container.encode(name, forKey: .name)
             try container.encode(position, forKey: .position)
             try container.encode(rotationAngle.degrees, forKey: .rotationAngle)
+            try container.encode(isSpecial, forKey: .isSpecial)
         }
         
-        init(id: UUID = UUID(), size: CGSize, name: String, position: CGPoint, rotationAngle: Angle) {
+        init(id: UUID = UUID(), size: CGSize, name: String, position: CGPoint, rotationAngle: Angle, isSpecial:Bool) {
             self.id = id
             self.size = size
             self.name = name
             self.position = position
             self.rotationAngle = rotationAngle
+            self.isSpecial = isSpecial
         }
         
         init(from decoder: Decoder) throws {
@@ -141,6 +154,7 @@ struct ContentView: View {
             self.position = try container.decode(CGPoint.self, forKey: .position)
             let rotationAngleInDegrees = try container.decode(Double.self, forKey: .rotationAngle)
             self.rotationAngle = Angle(degrees: rotationAngleInDegrees)
+            self.isSpecial = try container.decode(Bool.self, forKey: .isSpecial)
         }
     }
 
@@ -155,11 +169,11 @@ struct ContentView: View {
                 .padding()
 
                 if !isLocked {
-                    Button(action: { isPopoverPresented.toggle() }) {
+                    Button(action: { isPopoverPresented1.toggle() }) {
                         Image(systemName: "plus")
                     }
                     .padding()
-                    .popover(isPresented: $isPopoverPresented, content: {
+                    .popover(isPresented: $isPopoverPresented1, content: {
                         VStack {
                             HStack {
                                 TextField("Width", text: $widthText, onCommit: {})
@@ -178,14 +192,48 @@ struct ContentView: View {
                             Button("Add", action: {
                                 if let width = Double(widthText), let height = Double(heightText) {
                                     addRectangleButton(size: CGSize(width: width, height: height), name: newName)
-                                    isPopoverPresented = false // Dismiss popover after adding button
+                                    isPopoverPresented1 = false // Dismiss popover after adding button
                                 }
                             })
                             .padding()
                         }
                         .padding()
                     })
-                    
+                    Button(action: { isPopoverPresented2.toggle() }) {
+                        Image(systemName: "plus.circle")
+                    }
+                    .padding()
+                    .popover(isPresented: $isPopoverPresented2, content: {
+                        VStack {
+                            HStack {
+                                TextField("Width", text: $widthText, onCommit: {})
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                    .padding()
+                                
+                                TextField("Height", text: $heightText, onCommit: {})
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                    .padding()
+                            }
+                            
+                            Picker(selection: $selectedOption, label: Text("Select Name")) {
+                                ForEach(0..<names.count) { index in
+                                    Text(names[index])
+                                }
+                            }
+                            .pickerStyle(DefaultPickerStyle())
+                            .padding()
+                            
+                            Button("Add", action: {
+                                if let width = Double(widthText), let height = Double(heightText) {
+                                    addRectangleButtonSpecial(size: CGSize(width: width, height: height), name: names[selectedOption])
+                                    isPopoverPresented2 = false // Dismiss popover after adding button
+                                }
+                            })
+                            .padding()
+                        }
+                        .padding()
+                    })
+
                     Button(action: {
                         removeAllButtons()
                     }) {
@@ -225,7 +273,8 @@ struct ContentView: View {
                                     rotationAngle: self.$rectangleButtons[index].rotationAngle, // Pass binding
                                     isLocked: self.$isLocked,
                                     selectedButtonNames: self.$selectedButtonNames,
-                                    client:client)
+                                    client:client,
+                                    isSpecial: self.$rectangleButtons[index].isSpecial)
                     .position(rectangleButtons[index].position)
                         .rotationEffect(rectangleButtons[index].rotationAngle)
                 }
@@ -283,7 +332,27 @@ struct ContentView: View {
 
         let position = CGPoint(x: randomX, y: randomY)
         let rotationAngle = Angle(degrees: 0.0) // Initial rotation angle
-        rectangleButtons.append(RectangleButtonProperties(size: size, name: name, position: position, rotationAngle: rotationAngle))
+        rectangleButtons.append(RectangleButtonProperties(size: size, name: name, position: position, rotationAngle: rotationAngle,isSpecial:false))
+        widthText = "100"
+        heightText = "50"
+        newName = ""
+    }
+    
+    func addRectangleButtonSpecial(size: CGSize, name: String) {
+        let centerAreaWidth = UIScreen.main.bounds.width / 2
+        let centerAreaHeight = UIScreen.main.bounds.height / 2
+
+        let minX = (UIScreen.main.bounds.width - centerAreaWidth) / 2
+        let minY = (UIScreen.main.bounds.height - centerAreaHeight) / 2
+        let maxX = minX + centerAreaWidth - size.width
+        let maxY = minY + centerAreaHeight - size.height
+
+        let randomX = centerAreaWidth
+        let randomY = centerAreaHeight
+
+        let position = CGPoint(x: randomX, y: randomY)
+        let rotationAngle = Angle(degrees: 0.0) // Initial rotation angle
+        rectangleButtons.append(RectangleButtonProperties(size: size, name: name, position: position, rotationAngle: rotationAngle,isSpecial:true))
         widthText = "100"
         heightText = "50"
         newName = ""
